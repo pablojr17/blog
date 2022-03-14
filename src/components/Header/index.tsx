1490507497fb989dd117c076c76cbdb1797866e8
Pablo Gomes <pablojr7@gmail.com>
2022-03-14 11:23:34 -0300

    mudança de logo e header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import logo from "../../../public/images/logo.svg";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a>
+          <Image src={logo} alt="Pablo Gomes Logo" />
+        </a>
+
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+
+          <Link href="/posts">
+            <a>Conteúdos</a>
+          </Link>
+
+          <Link href="/sobre">
+            <a>Quem somos?</a>
+          </Link>
+        </nav>
+
+        <a className={styles.readyButton} type="button" href="#">
+          CONHECER
+        </a>
+      </div>
+    </header>
+  );
+}
