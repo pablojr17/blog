c6a1d338e7183adf2702d76aed4157fd73d50c6b
Pablo Gomes <pablojr7@gmail.com>
2022-03-17 17:23:23 -0300

    home 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,10 +25,40 @@ export default function Home() {
               </button>
             </a>
           </section>
-          {/* <img
+          <img
             src="/images/banner-conteudos.png"
             alt="Conteúdos Sujeito Programador"
-          /> */}
+          />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda a cria aplicativos para android e IOS</h2>
+            <span>
+              Você vai descobrir o jeito mais moderno de desenvolver app nativos
+              IOS e android
+            </span>
+          </section>
+
+          <img
+            src="/images/financasApp.png"
+            alt="Conteudos desenvolvimento de apps"
+          />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <img src="/images/webDev.png" alt="Aprenda criar sistemas web" />
+          <section>
+            <h2>Aprenda a cria aplicativos para android e IOS</h2>
+            <span>
+              Criar sistemas web é uma das tarefas mais importantes para
+              desenvolvedores.
+            </span>
+          </section>
         </div>
       </main>
     </>
