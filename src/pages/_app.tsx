add282ecd399d726a642e3cb5812383befa4fcd1
Pablo Gomes <pablojr7@gmail.com>
2022-04-04 23:21:56 -0300

    Alterações

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       ) : (
         <>
           <Header />
-          <Component {...pageProps} />;
+          <Component {...pageProps} />
         </>
       )}
     </>
