ae05a8df35265a964874026a1fbacb92f8de6b77
pablojr17 <pablojr17@gmail.com>
2022-07-13 10:52:01 -0300

    melhoria seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,12 @@
 import { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { FingerPrint } from "../components/FingerPrint";
 import { Header } from "../components/Header";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <FingerPrint />
-      ) : (
-        <>
-          <Header />
-          <Component {...pageProps} />
-        </>
-      )}
+      <Header />
+      <Component {...pageProps} />
     </>
   );
 }
